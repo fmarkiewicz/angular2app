36757cbcee1efc1eb9a23be9e1804b64912c71da
Fryderyk Markiewicz <fryderyk.markiewicz@agora.pl>
2018-01-02 16:13:02 +0100

    dynamiczne wlasciwosci 2:57

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { setImmediate, setInterval } from 'timers';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'witaj w kursie Angular!';
+
 }
